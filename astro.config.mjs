e549a12201d4f1122add971d8222919408a9048a
Dylan <dylan@mnsr.win>
2025-01-02 15:46:27 -0600

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// @ts-check
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: node({
+        mode: "standalone"
+    })
+});
